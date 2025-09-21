a746b4978090797c9340e80bd2363d79c28a3d38
bodeddy <107798427+bodeddy@users.noreply.github.com>
2025-09-21 13:13:48 +0100

    Add function for user choice
    A function allowing the user to choose from rock, paper, or
    scissors.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
-function getComputerChoice() {
-    const rock = "Rock";
-    const paper = "Paper";
-    const scissors = "Scissors";
-    
+const rock = "Rock";
+const paper = "Paper";
+const scissors = "Scissors";
+
+function getComputerChoice() {    
     let randomChoice = Math.random();
     if (randomChoice < 0.3) {
         return rock;
@@ -14,3 +14,19 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+
+function getHumanChoice() {
+    let userChoice = Number(prompt("Let's play rock, paper, scissors. For rock, type 1; for paper, type 2; and for scissors, type 3: "));
+    if (userChoice === 1) {
+        return rock;
+    } else if (userChoice === 2) {
+        return paper;
+    } else if (userChoice === 3) {
+        return scissors;
+    } else {
+        console.log("Wrong input. You had your chance and blew it.")
+    }
+}
+
+console.log(getHumanChoice());
