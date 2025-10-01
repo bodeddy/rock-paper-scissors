8daa39cc0ec712574d2d965591d5e364a4ddbaba
bodeddy <107798427+bodeddy@users.noreply.github.com>
2025-10-01 13:57:38 +0100

    Changes to plan and experimenting

--- planning.js
+++ planning.js
@@ -1,37 +1,61 @@
-// Plan for rock, paper, scissors
-
-// simple version:
+/*
+let userChoice;
+let compChoice;
 
 // player to input rock or paper
-
-let userChoice = prompt("Please choose rock or paper: ");
-
-console.log(userChoice);
-
+function getHumanChoice() {
+    let userChoice = prompt("Please choose rock or paper: ");
+    console.log(userChoice);
+}
 
 // computer to randomly choose rock or paper
 
-let compChoice = Math.random();
-if (compChoice < 0.5) {
-    compChoice = "rock";
-    } else {
-    compChoice = "paper";
+function getComputerChoice() {
+    let compChoice = Math.random();
+    if (compChoice < 0.5) {
+        compChoice = "rock";
+        } else {
+        compChoice = "paper";
+    }
+    console.log(compChoice);
 }
 
-console.log(compChoice);
-
 // if player rock and computer rock, draw
-
-if (userChoice === compChoice) {
-    console.log(`You both chose ${userChoice}. It's a draw.`);
-} else if (userChoice === "rock" && compChoice === "paper") {
-    console.log("Unlucky. You lose.");
-} else {
-    console.log("You win!");
+function playRound(human, comp) {
+    if (human === comp) {
+        console.log(`You both chose ${userChoice}. It's a draw.`);
+    } else if (human === "rock" && comp === "paper") {
+        console.log("Unlucky. You lose.");
+    } else {
+        console.log("You win!");
+    }
 }
 
 // if player rock and computer paper, computer wins
 
 // if player paper and computer rock, player wins
 
+// Increase score for winner
+
 // declare winner
+
+// initiate code
+
+let humanSelection = getHumanChoice();
+let compSelection = getComputerChoice();
+
+playRound(humanSelection, compSelection);
+
+*/
+
+
+let human = "rock";
+let comp = "paper";
+
+if (human === comp) {
+    console.log("Draw");
+} else if (human === "rock" && comp === "paper") {
+    console.log("You lose");
+} else {
+    console.log("You win");
+}
