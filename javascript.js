a3f0f037e736d931bef71bf0fe7407b97d032d4e
bodeddy <107798427+bodeddy@users.noreply.github.com>
2025-10-01 21:24:11 +0100

    Add function that enables 5 rounds of play
    Although I've been successful in getting this to work, I think the
    playGame function is probably too big and needs to be broken into
    smaller functions that do less things (though, of course, I could be
    wrong about this!).

--- javascript.js
+++ javascript.js
@@ -5,12 +5,11 @@ const scissors = "scissors";
 let humanScore = 0;
 let computerScore = 0;
 
-
 function getComputerChoice() {    
     let randomChoice = Math.random();
     if (randomChoice < 0.3) {
         return rock;
-    } else if (randomChoice >= 0.3 && randomChoice < 0.6){
+    } else if (randomChoice >= 0.3 && randomChoice < 0.6) {
         return paper;
     } else {
         return scissors;
@@ -23,27 +22,32 @@ function getHumanChoice() {
     return caseUserChoice;
 }
 
-// const computerChoice = getComputerChoice();
-// const humanChoice = getHumanChoice();
 
-function playRound(computer, human) {    
-    if (human === computer) {
-        console.log(`You chose ${human} and the computer chose ${computer}. It's a draw.`);
-    } else if (
-        human === "rock" && computer === "paper" || 
-        human === "scissors" && computer === "rock" ||
-        human === "paper" && computer === "scissors") {
-        console.log(`You chose ${human} and the computer chose ${computer}. You lose!`);
-        computerScore++;
-    } else {
-        console.log(`You chose ${human} and the computer chose ${computer}. You win!`);
-        humanScore++;
+function playGame() {
+    
+    function playRound(computer, human) {    
+        if (human === computer) {
+            console.log(`You chose ${human} and the computer chose ${computer}. It's a draw.`);
+        } else if (
+            human === "rock" && computer === "paper" || 
+            human === "scissors" && computer === "rock" ||
+            human === "paper" && computer === "scissors") {
+            console.log(`You chose ${human} and the computer chose ${computer}. You lose!`);
+            computerScore++;
+        } else {
+            console.log(`You chose ${human} and the computer chose ${computer}. You win!`);
+            humanScore++;
+        }
+    }
+    
+    for (let i = 0; i < 5; i++) {
+        const computerChoice = getComputerChoice();
+        const humanChoice = getHumanChoice();
+        playRound(computerChoice, humanChoice);
+        console.log(`Computer: ${computerScore}`);
+        console.log(`Human: ${humanScore}`);
     }
 }
 
-playRound(getComputerChoice(), getHumanChoice());
-
-
-console.log(`Computer: ${computerScore}`);
-console.log(`Human: ${humanScore}`);
+playGame();
 
