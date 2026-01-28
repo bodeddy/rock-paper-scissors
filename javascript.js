e92427d291356008d9492f8feac52dd2d93ff774
bodeddy <107798427+bodeddy@users.noreply.github.com>
2026-01-28 07:22:20 +0000

    Fix random computer choice
    Before, although once a button was clicked it chose a random option for
    the computer, it kept that choice when the user continued to press the
    other buttons. Now, it doesn't do this. So every time a button is
    pressed, a new computer choice is generated.

--- javascript.js
+++ javascript.js
@@ -19,33 +19,36 @@ function getComputerChoice() {
 const messageDiv = document.querySelector("#messages");
 const displayDiv = document.createElement("div");
     
-function playRound(computer, human) {    
+function playRound(computer, human) { 
     if (human === computer) {
         messageDiv.appendChild(displayDiv);
-        displayDiv.textContent = `You chose ${human} and the computer chose ${computer}. It's a draw.`;
+        displayDiv.textContent = `You chose ${human} and the computer chose ${computer}. It's a draw. Score: ${humanScore} : ${computerScore}`;
     } else if (
         human === "rock" && computer === "paper" || 
         human === "scissors" && computer === "rock" ||
         human === "paper" && computer === "scissors") {
         messageDiv.appendChild(displayDiv);
-        displayDiv.textContent = `You chose ${human} and the computer chose ${computer}. You lose!`;
+        displayDiv.textContent = `You chose ${human} and the computer chose ${computer}. You lose! Score: ${humanScore} : ${computerScore}`;
         computerScore++;
     } else {
         messageDiv.appendChild(displayDiv);
-        displayDiv.textContent = `You chose ${human} and the computer chose ${computer}. You win!`;
+        displayDiv.textContent = `You chose ${human} and the computer chose ${computer}. You win! Score: ${humanScore} : ${computerScore}`;
         humanScore++;
     }
+
+    if (humanScore === 5) alert("Finally! You've gotten to 5!")
+
 }
 
-const computerChoice = getComputerChoice();
+// const computerChoice = getComputerChoice();
 
 const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
 const scissorsButton = document.querySelector("#scissors");
 
-rockButton.addEventListener("click", () => {playRound(computerChoice, rock)});
-paperButton.addEventListener("click", () => {playRound(computerChoice, paper)});
-scissorsButton.addEventListener("click", () => {playRound(computerChoice, scissors)});
+rockButton.addEventListener("click", () => {playRound(getComputerChoice(), rock)});
+paperButton.addEventListener("click", () => {playRound(getComputerChoice(), paper)});
+scissorsButton.addEventListener("click", () => {playRound(getComputerChoice(), scissors)});
 
 
 
