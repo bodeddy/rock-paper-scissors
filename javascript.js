8a7ca419a2d323520c4c0a44cf1ef9fd76bf06ab
bodeddy <107798427+bodeddy@users.noreply.github.com>
2026-01-28 19:27:54 +0000

    Add first to 5 points
    This took me some time, but I'm glad I've finally managed it. The code
    is messy, and I know (despite my inexperience) that this is not good
    code. But I imagine I can refactor it later down the line when I've
    acquired more knowledge. In the meantime, the game functions exactly
    as intended: rock paper scissors against the computer, with the first
    to 5 winning, and a message confirming who won.

--- javascript.js
+++ javascript.js
@@ -5,6 +5,17 @@ const scissors = "scissors";
 let humanScore = 0;
 let computerScore = 0;
 
+const messageDiv = document.querySelector("#messages");
+const displayDiv = document.createElement("div");
+
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+
+rockButton.addEventListener("click", () => {playRound(getComputerChoice(), rock)});
+paperButton.addEventListener("click", () => {playRound(getComputerChoice(), paper)});
+scissorsButton.addEventListener("click", () => {playRound(getComputerChoice(), scissors)});
+
 function getComputerChoice() {    
     let randomChoice = Math.random();
     if (randomChoice < 0.3) {
@@ -15,9 +26,6 @@ function getComputerChoice() {
         return scissors;
     }
 }
-
-const messageDiv = document.querySelector("#messages");
-const displayDiv = document.createElement("div");
     
 function playRound(computer, human) { 
     if (human === computer) {
@@ -36,19 +44,21 @@ function playRound(computer, human) {
         displayDiv.textContent = `You chose ${human} and the computer chose ${computer}. You win! Score: ${humanScore} : ${computerScore}`;
     }
 
-    if (humanScore === 5) alert("Finally! You've gotten to 5!")
+    if (humanScore === 5) {
+        messageDiv.appendChild(displayDiv);
+        displayDiv.textContent = "Well done! You win!";
+        rockButton.remove();
+        paperButton.remove();
+        scissorsButton.remove();
+    };
+
+    if (computerScore === 5) {
+        messageDiv.appendChild(displayDiv);
+        displayDiv.textContent = "Unlucky! The computer has won and is therefore superior to you.";
+        rockButton.remove();
+        paperButton.remove();
+        scissorsButton.remove();
+    }
 
 }
-
-// const computerChoice = getComputerChoice();
-
-const rockButton = document.querySelector("#rock");
-const paperButton = document.querySelector("#paper");
-const scissorsButton = document.querySelector("#scissors");
-
-rockButton.addEventListener("click", () => {playRound(getComputerChoice(), rock)});
-paperButton.addEventListener("click", () => {playRound(getComputerChoice(), paper)});
-scissorsButton.addEventListener("click", () => {playRound(getComputerChoice(), scissors)});
-
-
 
